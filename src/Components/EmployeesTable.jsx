a4da124c415f2d936657ffba96bf3d88c0096cca
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-02-08 12:17:15 +0530

    first api call

--- src/Components/EmployeesTable.jsx
+++ src/Components/EmployeesTable.jsx
@@ -2,47 +2,59 @@ import { useEffect, useState } from "react";
 import { fetchEmployeesDb } from "../Services/EmployeeServices";
 
 function EmployeesTable(){
-    const[db, setDb] = useState([]);
+    const [db, setDb] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [errorMsg, setErrorMsg] = useState(null);
 
     useEffect(()=>{
         fetchEmployeesDb()
         .then(response => {
             setDb(response.data);
+            setLoading(false);
         })
-        .catch(console.log);
+        .catch(err => {
+            setErrorMsg(err.message);
+            setLoading(false);
+        });
     },[]);
 
     return(
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        Employee Id
-                    </th>
-                    <th>
-                        Employee Name
-                    </th>
-                    <th>
-                        E-mail
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {db.map(employeeObj => (
-                    <tr key={employeeObj.id}>
-                        <td>
-                            {employeeObj.id}
-                        </td>
-                        <td>
-                            {`${employeeObj.firstName} ${employeeObj.lastName}`}
-                        </td>
-                        <td>
-                            {employeeObj.email}
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            {loading && <h3>Loading</h3>}
+            {errorMsg && <h3>Error loading data</h3>}
+            {db && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Employee Id
+                            </th>
+                            <th>
+                                Employee Name
+                            </th>
+                            <th>
+                                E-mail
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {db.map(employeeObj => (
+                            <tr key={employeeObj.id}>
+                                <td>
+                                    {employeeObj.id}
+                                </td>
+                                <td>
+                                    {`${employeeObj.firstName} ${employeeObj.lastName}`}
+                                </td>
+                                <td>
+                                    {employeeObj.email}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
     );
 }
 
